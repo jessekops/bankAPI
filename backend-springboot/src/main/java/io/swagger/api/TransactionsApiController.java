85a1df7b2633aee344fd5ef964c12fa19ae4ec83
662781 <662781@student.inholland.nl>
2022-05-04 12:57:09 +0200

    First Stub from V1.3

---
+++ backend-springboot/src/main/java/io/swagger/api/TransactionsApiController.java
@@ -0,0 +1,101 @@
+package io.swagger.api;
+
+import io.swagger.annotations.Api;
+import io.swagger.model.TransactionDTO;
+import java.util.UUID;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-04T10:45:44.270Z[GMT]")
+@RestController
+@Api(tags = {"Customer", "Employee"})
+public class TransactionsApiController implements TransactionsApi {
+
+    private static final Logger log = LoggerFactory.getLogger(TransactionsApiController.class);
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public TransactionsApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    public ResponseEntity<TransactionDTO> createTransaction(@Parameter(in = ParameterIn.DEFAULT, description = "New transaction object", required=true, schema=@Schema()) @Valid @RequestBody TransactionDTO body) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<TransactionDTO>(objectMapper.readValue("{\n  \"amount\" : 3.2,\n  \"userPerforming\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\",\n  \"from\" : \"NLxxINHO0xxxxxxxxx\",\n  \"id\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\",\n  \"to\" : \"NLxxINHO0xxxxxxxxx\",\n  \"timestamp\" : \"2000-01-23T04:56:07.000+00:00\"\n}", TransactionDTO.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<TransactionDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<TransactionDTO>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<Void> deleteTransaction(@Parameter(in = ParameterIn.PATH, description = "Transaction ID input", required=true, schema=@Schema()) @PathVariable("id") UUID id) {
+        String accept = request.getHeader("Accept");
+        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<TransactionDTO> getTransaction(@Parameter(in = ParameterIn.PATH, description = "Transaction ID input", required=true, schema=@Schema()) @PathVariable("id") UUID id) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<TransactionDTO>(objectMapper.readValue("{\n  \"amount\" : 3.2,\n  \"userPerforming\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\",\n  \"from\" : \"NLxxINHO0xxxxxxxxx\",\n  \"id\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\",\n  \"to\" : \"NLxxINHO0xxxxxxxxx\",\n  \"timestamp\" : \"2000-01-23T04:56:07.000+00:00\"\n}", TransactionDTO.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<TransactionDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<TransactionDTO>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<TransactionDTO> updateTransaction(@Parameter(in = ParameterIn.PATH, description = "Transaction ID input", required=true, schema=@Schema()) @PathVariable("id") UUID id,@Parameter(in = ParameterIn.DEFAULT, description = "Updated transaction object", required=true, schema=@Schema()) @Valid @RequestBody TransactionDTO body) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<TransactionDTO>(objectMapper.readValue("{\n  \"amount\" : 3.2,\n  \"userPerforming\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\",\n  \"from\" : \"NLxxINHO0xxxxxxxxx\",\n  \"id\" : \"d290f1ee-6c54-4b01-90e6-d701748f0851\",\n  \"to\" : \"NLxxINHO0xxxxxxxxx\",\n  \"timestamp\" : \"2000-01-23T04:56:07.000+00:00\"\n}", TransactionDTO.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<TransactionDTO>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<TransactionDTO>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+}
---
+++ backend-springboot/src/main/java/io/swagger/model/TransactionDTO.java
@@ -0,0 +1,207 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.UUID;
+import org.threeten.bp.OffsetDateTime;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * TransactionDTO
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-05-04T10:45:44.270Z[GMT]")
+
+
+public class TransactionDTO   {
+  @JsonProperty("id")
+  private UUID id = null;
+
+  @JsonProperty("timestamp")
+  private OffsetDateTime timestamp = null;
+
+  @JsonProperty("from")
+  private String from = null;
+
+  @JsonProperty("to")
+  private String to = null;
+
+  @JsonProperty("amount")
+  private Double amount = null;
+
+  @JsonProperty("userPerforming")
+  private UUID userPerforming = null;
+
+  public TransactionDTO id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   **/
+  @Schema(example = "d290f1ee-6c54-4b01-90e6-d701748f0851", description = "")
+  
+    @Valid
+    public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public TransactionDTO timestamp(OffsetDateTime timestamp) {
+    this.timestamp = timestamp;
+    return this;
+  }
+
+  /**
+   * Get timestamp
+   * @return timestamp
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public OffsetDateTime getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(OffsetDateTime timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public TransactionDTO from(String from) {
+    this.from = from;
+    return this;
+  }
+
+  /**
+   * Get from
+   * @return from
+   **/
+  @Schema(example = "NLxxINHO0xxxxxxxxx", required = true, description = "")
+      @NotNull
+
+    public String getFrom() {
+    return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  public TransactionDTO to(String to) {
+    this.to = to;
+    return this;
+  }
+
+  /**
+   * Get to
+   * @return to
+   **/
+  @Schema(example = "NLxxINHO0xxxxxxxxx", required = true, description = "")
+      @NotNull
+
+    public String getTo() {
+    return to;
+  }
+
+  public void setTo(String to) {
+    this.to = to;
+  }
+
+  public TransactionDTO amount(Double amount) {
+    this.amount = amount;
+    return this;
+  }
+
+  /**
+   * Amount of Euros
+   * @return amount
+   **/
+  @Schema(example = "3.2", required = true, description = "Amount of Euros")
+      @NotNull
+
+    public Double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Double amount) {
+    this.amount = amount;
+  }
+
+  public TransactionDTO userPerforming(UUID userPerforming) {
+    this.userPerforming = userPerforming;
+    return this;
+  }
+
+  /**
+   * Get userPerforming
+   * @return userPerforming
+   **/
+  @Schema(example = "d290f1ee-6c54-4b01-90e6-d701748f0851", description = "")
+  
+    @Valid
+    public UUID getUserPerforming() {
+    return userPerforming;
+  }
+
+  public void setUserPerforming(UUID userPerforming) {
+    this.userPerforming = userPerforming;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    TransactionDTO transactionDTO = (TransactionDTO) o;
+    return Objects.equals(this.id, transactionDTO.id) &&
+        Objects.equals(this.timestamp, transactionDTO.timestamp) &&
+        Objects.equals(this.from, transactionDTO.from) &&
+        Objects.equals(this.to, transactionDTO.to) &&
+        Objects.equals(this.amount, transactionDTO.amount) &&
+        Objects.equals(this.userPerforming, transactionDTO.userPerforming);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, timestamp, from, to, amount, userPerforming);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class TransactionDTO {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    timestamp: ").append(toIndentedString(timestamp)).append("\n");
+    sb.append("    from: ").append(toIndentedString(from)).append("\n");
+    sb.append("    to: ").append(toIndentedString(to)).append("\n");
+    sb.append("    amount: ").append(toIndentedString(amount)).append("\n");
+    sb.append("    userPerforming: ").append(toIndentedString(userPerforming)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
